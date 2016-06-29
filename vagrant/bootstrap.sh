69124158e0e2325a47e693f7d7653429c416a789
RadicalErin <erin.eileen.scanlon@gmail.com>
2016-06-29 22:46:19 +0000

    Splitting vagrant boot scripts into first and always

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,19 +1,8 @@
 sudo su
-# symfony
-curl -Ls https://symfony.com/installer -o /usr/local/bin/symfony
-chmod a+x /usr/local/bin/symfony
-# composer
-curl -s https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-composer self-update
-# web directory
-mkdir -p www
-cd www
 # update the machine
 apt-get update
-# lamp server
-apt-get install lamp-server^
+# composer
+composer self-update
 # make sure we own the vagrant files in the shared folder
+cd www
 chown -R vagrant:vagrant .*
-# set php time zone here
-service apache2 restart
-
---
+++ vagrant/firstboot.sh
@@ -0,0 +1,24 @@
+sudo su
+# update the machine
+apt-get update
+# symfony
+if ! hash symfony 2>/dev/null; then
+    curl -Ls https://symfony.com/installer -o /usr/local/bin/symfony
+    chmod a+x /usr/local/bin/symfony
+fi
+# composer
+if ! hash composer 2>/dev/null; then
+    curl -s https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+fi
+composer self-update
+# web directory
+mkdir -p www
+cd www
+# lamp server
+apt-get -y --force-yes install lamp-server^
+# make sure we own the vagrant files in the shared folder
+chown -R vagrant:vagrant .*
+# set php time zone here
+service apache2 restart
+# install gui ... comment to disable
+apt-get -y --force-yes install ubuntu-desktop
