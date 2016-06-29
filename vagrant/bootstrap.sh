badcf1df383aaee7974bb754dbb3641e6332ab82
RadicalErin <erin.eileen.scanlon@gmail.com>
2016-06-29 08:41:31 +0000

    Adding a (currently temporary) bootstrap file

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,19 @@
+sudo su
+# symfony
+curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony
+chmod a+x /usr/local/bin/symfony
+# composer
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+composer self-update
+# web directory
+mkdir -p www
+cd www
+# update the machine
+apt-get update
+# lamp server
+apt-get install lamp-server^
+# make sure we own the vagrant files in the shared folder
+chown -R vagrant:vagrant .*
+# set php time zone here
+service apache2 restart
+
