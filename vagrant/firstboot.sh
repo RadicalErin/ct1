1f53b60560a03faf39357801baf3549a5c12ecc2
RadicalErin <erin.eileen.scanlon@gmail.com>
2016-06-30 23:31:35 +0000

    Adding setfacl commands to firstboot.sh

--- vagrant/firstboot.sh
+++ vagrant/firstboot.sh
@@ -16,6 +16,9 @@ apt-get -y --force-yes install lamp-server^
 # make sure we own the vagrant files in the shared folder
 chown -R vagrant:vagrant .*
 # set php time zone here
+HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
+setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
+setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
 service apache2 restart
 # install gui ... comment to disable
 apt-get -y --force-yes install ubuntu-desktop
