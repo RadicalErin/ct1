80d932e5dbba26319438df8411068af27fe4a1c5
RadicalErin <erin.eileen.scanlon@gmail.com>
2016-07-04 03:16:51 +0000

    Create empty soap controller

--- src/ct1/src/AppBundle/Controller/SoapController.php
+++ src/ct1/src/AppBundle/Controller/SoapController.php
@@ -6,7 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * @Route("/soap")
+ */
 class SoapController extends Controller
 {
+    /**
+     * @route(
+     *     "/",
+     *     name="soap_index"
+     * )
+     */
+    public function indexAction()
+    {
+        //run the soap server in non wsdl mode
+        $server = new \SoapServer(null);
+    }
 }
 
