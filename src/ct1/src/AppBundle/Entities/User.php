e54f6c077eec35290b9b1cca8fd9a13668d21f90
RadicalErin <erin.eileen.scanlon@gmail.com>
2016-07-04 04:17:58 +0000

    Create user entity

---
+++ src/ct1/src/AppBundle/Entities/User.php
@@ -0,0 +1,61 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+class User
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", Length=128)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", Length=255)
+     */
+    private $password;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword(password)
+    {
+        $this->password = $password;
+        return $this;
+    } 
+}
