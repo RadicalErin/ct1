630a78af5e115028faa55d066130a0079321c568
RadicalErin <erin.eileen.scanlon@gmail.com>
2016-07-05 21:34:03 +0000

    Setting up user entity to support authentication

--- src/ct1/src/AppBundle/Entity/User.php
+++ src/ct1/src/AppBundle/Entity/User.php
@@ -3,18 +3,20 @@ namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as FormValidator;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\EquatableInterface;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="users")
  */
-class User extends BaseEntity
+class User extends BaseEntity implements UserInterface, EquatableInterface
 {
     /**
      * @ORM\Column(type="string", length=128)
      * @FormValidator\NotBlank()
      */
-    private $name;
+    private $username;
 
     /**
      * @ORM\Column(type="string", length=255)
@@ -22,14 +24,26 @@ class User extends BaseEntity
      */
     private $password;
 
-    public function getName()
+    private $salt;
+
+    private $roles;
+
+    public function __construct($username, $password, $salt, array $roles)
     {
-        return $this->name;
+        $this->username = $username;
+        $this->password = $password;
+        $this->salt = $salt;
+        $this->roles = $roles;
     }
 
-    public function setName($name)
+    public function getUsername()
     {
-        $this->name = $name;
+        return $this->username;
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
         return $this;
     }
 
@@ -42,5 +56,44 @@ class User extends BaseEntity
     {
         $this->password = $password;
         return $this;
-    } 
+    }
+
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+        return $this;
+    }
+
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    public function setRoles($roles)
+    {
+        $this->roles = $roles;
+        return $this;
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    public function isEqualTo(UserInterface $user)
+    {
+        if(
+            $user instanceof WebserviceUser &&
+            $this->password == $user->getPassword() &&
+            $this->salt == $user->getSalt() &&
+            $this->username == $user->getUsername()
+        ){
+            return true;
+        }
+        return false;
+    }
 }
