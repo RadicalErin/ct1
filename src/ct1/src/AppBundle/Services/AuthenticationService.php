dc0465e8a2e3ff09421d38cbfb522c3a1725c185
RadicalErin <erin.eileen.scanlon@gmail.com>
2016-07-05 20:07:38 +0000

    Create the service functions

---
+++ src/ct1/src/AppBundle/Services/AuthenticationService.php
@@ -0,0 +1,17 @@
+<?php
+namespace AppBundle\Services;
+
+class AuthenticationService
+{
+    public function logIn($submittedName, $submittedPassword)
+    {
+    }
+
+    public function logOut($userName)
+    {
+    }
+
+    public function newUser($submittedName, $submittedPassword)
+    {
+    }
+}
