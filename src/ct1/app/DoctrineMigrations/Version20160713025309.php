1b6b52ad38875ab0392f8db05e7c8e09b7facdd0
RadicalErin <erin.eileen.scanlon@gmail.com>
2016-07-13 07:59:49 +0000

    Consolidating migrations, since no data has been created yet, and there is a bug in sqlite3

--- src/ct1/app/DoctrineMigrations/Version20160713025309.php
+++ src/ct1/app/DoctrineMigrations/Version20160713025309.php
@@ -8,17 +8,16 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20160704002006 extends AbstractMigration
+class Version20160713025309 extends AbstractMigration
 {
     /**
      * @param Schema $schema
      */
     public function up(Schema $schema)
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
-
-        $this->addSql('CREATE TABLE users (id INTEGER NOT NULL, name VARCHAR(128) NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE post (id INTEGER NOT NULL, created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE users (id INTEGER NOT NULL, username VARCHAR(128) NOT NULL, password VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(id))');
     }
 
     /**
@@ -26,9 +25,8 @@ class Version20160704002006 extends AbstractMigration
      */
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
-
+        $this->addSql('DROP TABLE post');
         $this->addSql('DROP TABLE users');
     }
 }
