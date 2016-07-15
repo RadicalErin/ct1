5c73df83ebcf4d413d9354a4142618083e9a5272
RadicalErin <erin.eileen.scanlon@gmail.com>
2016-07-15 13:08:17 +0000

    Solving another sqlite frustration

--- src/ct1/app/DoctrineMigrations/Version20160713025309.php
+++ src/ct1/app/DoctrineMigrations/Version20160713025309.php
@@ -16,8 +16,9 @@ class Version20160713025309 extends AbstractMigration
     public function up(Schema $schema)
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
-        $this->addSql('CREATE TABLE post (id INTEGER NOT NULL, created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE users (id INTEGER NOT NULL, username VARCHAR(128) NOT NULL, password VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE post (id INTEGER NOT NULL, author INTEGER NOT NULL, text CLOB NOT NULL, created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(id), FOREIGN KEY(author) REFERENCES users(id))');
+        $this->addSql('CREATE INDEX IDX_5A8A6C8DBDAFD8C8 ON post (author)');
     }
 
     /**
--- src/ct1/src/AppBundle/Entity/Post.php
+++ src/ct1/src/AppBundle/Entity/Post.php
@@ -2,6 +2,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use AppBundle\Entity\User; 
 
 /**
  * @ORM\Entity
@@ -9,4 +10,35 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Post extends BaseEntity
 {
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $text;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\JoinColumn(name="author", referencedColumnName="id", nullable=false)
+     */
+    private $author;
+
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    public function setText($text)
+    {
+        $this->text = $text;
+        return $this;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
 }
