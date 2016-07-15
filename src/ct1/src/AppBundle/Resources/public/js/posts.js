c93c516418737a07755ab357a32bdc41581ac82e
RadicalErin <erin.eileen.scanlon@gmail.com>
2016-07-15 17:37:28 +0000

    Beginning to flesh out the js side of the rest call

--- src/ct1/src/AppBundle/Resources/public/js/posts.js
+++ src/ct1/src/AppBundle/Resources/public/js/posts.js
@@ -3,6 +3,31 @@ var postManager = {
         this.run();
     },
     run: function(){
+        $('#post_Post').on("click", function(e){
+            postManager.submitNewPost();
+        });
+    },
+    submitNewPost: function(){
+        $.ajax({
+            url: 'dunno',
+            data: {
+                user: authenticationManager.config.user,
+                content: $('#post_Content').val()
+            },
+            type: "GET",
+            dataTye: "json"
+        })
+        .done(function(result){
+            //put new post content in place
+        })
+        .fail(function(xhr, status, err){
+            alert("Unable to post the message");
+        });
+    },
+    updatePostWindow: function(newHtml){
+    },
+    clearForm: function(){
+        $('#post_Content').val('');
     }
 }
 
